6f20ab7c13cfb0b4ce6b0f668ad60ead3b40a4ef
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-10-13 21:54:58 +0700

    refactor!: add responsiveness and fixed navgitation

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -3,7 +3,7 @@
 import classes from "./logo.module.css";
 
 function Logo() {
-	return <div className={classes.logo}>Saddam' Next Blog</div>;
+	return <div className={classes.logo}>Saddam Next Blog</div>;
 }
 
 export default Logo;
